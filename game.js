3e7ab0921f3c6cbfd0fc6aaa060da1ef23461513
TheTravellingVetPhysio <julietherold@hotmail.com>
2025-11-18 18:08:50 +0100

    ok

--- game.js
+++ game.js
@@ -1,14 +1,17 @@
+import { gameScenario } from "./scenarios.js";
+
 //
 // Variables
 //
 
-
+const modeAdults = document.querySelector("#adult-mode");
+const modeKids = document.querySelector("#kids-mode");
+let mode = null;
 
 //
 // Methods & Functions
 //
 
-import { gameScenario } from "./scenarios";
 
 function startGame(version) {
     const config = gameScenario[version];
@@ -19,3 +22,15 @@ function startGame(version) {
 //
 // Inits & Event Listeners
 //
+
+versionAdults.addEventListener("click", () => {
+    startGame('adult');
+    document.getElementById('welcome').style.display = 'none';
+    document.getElementById('setup').style.display = 'block';
+});
+
+versionKids.addEventListener("click", () => {
+    startGame('kids');
+    document.getElementById('welcome').style.display = 'none';
+    document.getElementById('setup').style.display = 'block';
+});
