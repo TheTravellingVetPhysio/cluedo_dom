1fb9a9b4caf3f6b2e0159c9dbde1f034074479e1
TheTravellingVetPhysio <julietherold@hotmail.com>
2025-11-19 10:08:18 +0100

    setup layout finished

--- game.js
+++ game.js
@@ -1,12 +1,20 @@
 import { gameScenario } from "./scenarios.js";
 
 // =========================
-// 1. SELECT MODE
+// 0. HIDDEN ELEMENTS
 // =========================
 
 document.getElementById("setup").classList.add("hidden");
 document.getElementById("gameplay").classList.add("hidden");
 document.getElementById("results").classList.add("hidden");
+document.getElementById("intro2").classList.add("hidden");
+document.getElementById("intro3").classList.add("hidden");
+document.getElementById("playersetup").classList.add("hidden");
+document.getElementById("startgamebutton").classList.add("hidden");
+
+// =========================
+// 1. SELECT MODE
+// =========================
 
 let mode = null;
 let config = null;
@@ -23,17 +31,31 @@ function selectMode(selectedMode) {
   config = gameScenario[mode];
 
   localStorage.setItem("gameMode", mode);
-  
+
   document.getElementById("welcome").classList.add("hidden");
   document.getElementById("setup").classList.remove("hidden");
   document.getElementById("intro1").innerHTML = config.prompts.intro1;
-  document.getElementById("intro1").style.fontFamily = mode === "adults" ? '"Oooh Baby", Georgia, "Times New Roman", Times, serif' : '"Reenie Beanie", "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande", "Lucida Sans", Arial, sans-serif';
+  document.getElementById("intro1").style.fontFamily =
+    mode === "adults"
+      ? '"Oooh Baby", Georgia, "Times New Roman", Times, serif'
+      : '"Reenie Beanie", "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande", "Lucida Sans", Arial, sans-serif';
+  setTimeout(() => {
+    document.getElementById("intro2").innerHTML = config.prompts.intro2;
+    document.getElementById("intro2").classList.remove("hidden");
+  }, 2000);
+  setTimeout(() => {
+    document.getElementById("intro3").innerHTML = config.prompts.intro3;
+    document.getElementById("intro3").classList.remove("hidden");
+  }, 3500);
+  setTimeout(() => {
+    document.getElementById("playersetup").classList.remove("hidden");
+    document.getElementById("startgamebutton").classList.remove("hidden");
+  }, 5000);
 }
 
 // =========================
 // 2. SETUP GAME
 // =========================
-
 
 // TO DO: import players
 
