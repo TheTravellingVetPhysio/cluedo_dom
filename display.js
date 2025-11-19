1b2efdb236372e1c83ab3de7a4f3451295bf9cea
TheTravellingVetPhysio <julietherold@hotmail.com>
2025-11-19 10:42:55 +0100

    display handles transistionToSetup

--- display.js
+++ display.js
@@ -3,15 +3,40 @@
 // =============================
 
 export function hideSectionsInitially() {
-document.getElementById("setup").classList.add("hidden");
-document.getElementById("gameplay").classList.add("hidden");
-document.getElementById("results").classList.add("hidden");
-document.getElementById("intro2").classList.add("hidden");
-document.getElementById("intro3").classList.add("hidden");
-document.getElementById("playersetup").classList.add("hidden");
-document.getElementById("startgamebutton").classList.add("hidden");
+  document.getElementById("setup").classList.add("hidden");
+  document.getElementById("gameplay").classList.add("hidden");
+  document.getElementById("results").classList.add("hidden");
+  document.getElementById("intro2").classList.add("hidden");
+  document.getElementById("intro3").classList.add("hidden");
+  document.getElementById("playersetup").classList.add("hidden");
+  document.getElementById("startgamebutton").classList.add("hidden");
 }
 
 // =============================
-// 0. HIDDEN ELEMENTS FROM START
+// 1. SELECT
+// =============================
+
+export function transitionToSetup() {
+  document.getElementById("welcome").classList.add("hidden");
+  document.getElementById("setup").classList.remove("hidden");
+  document.getElementById("intro1").innerHTML = config.prompts.intro1;
+  document.getElementById("intro1").style.fontFamily =
+    mode === "adults"
+      ? '"Oooh Baby", Georgia, "Times New Roman", Times, serif'
+      : '"Reenie Beanie", "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande", "Lucida Sans", Arial, sans-serif';
+  setTimeout(() => {
+    document.getElementById("intro2").innerHTML = config.prompts.intro2;
+    document.getElementById("intro2").classList.remove("hidden");
+  }, 2000);
+  setTimeout(() => {
+    document.getElementById("intro3").innerHTML = config.prompts.intro3;
+    document.getElementById("intro3").classList.remove("hidden");
+  }, 3500);
+  setTimeout(() => {
+    document.getElementById("playersetup").classList.remove("hidden");
+  }, 5000);
+}
+
+// =============================
+// 2. SETUP
 // =============================
--- game.js
+++ game.js
@@ -23,24 +23,7 @@ function selectMode(selectedMode) {
 
   localStorage.setItem("gameMode", mode);
 
-  document.getElementById("welcome").classList.add("hidden");
-  document.getElementById("setup").classList.remove("hidden");
-  document.getElementById("intro1").innerHTML = config.prompts.intro1;
-  document.getElementById("intro1").style.fontFamily =
-    mode === "adults"
-      ? '"Oooh Baby", Georgia, "Times New Roman", Times, serif'
-      : '"Reenie Beanie", "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande", "Lucida Sans", Arial, sans-serif';
-  setTimeout(() => {
-    document.getElementById("intro2").innerHTML = config.prompts.intro2;
-    document.getElementById("intro2").classList.remove("hidden");
-  }, 2000);
-  setTimeout(() => {
-    document.getElementById("intro3").innerHTML = config.prompts.intro3;
-    document.getElementById("intro3").classList.remove("hidden");
-  }, 3500);
-  setTimeout(() => {
-    document.getElementById("playersetup").classList.remove("hidden");
-  }, 5000);
+  display.transitionToSetup();
 }
 
 // =========================
@@ -50,20 +33,21 @@ function selectMode(selectedMode) {
 // TO DO: import players
 
 const inputs = document.querySelectorAll('#playersetup input[type="text"]');
-const playButton = document.getElementById('startgamebutton');
+const playButton = document.getElementById("startgamebutton");
 
 function checkInputs() {
-  const hasValue = Array.from(inputs).some(input => input.value.trim() !== '');
+  const hasValue = Array.from(inputs).some(
+    (input) => input.value.trim() !== ""
+  );
   if (hasValue) {
-    playButton.classList.remove('hidden');
-  }
-  else {
-    playButton.classList.add('hidden')
+    playButton.classList.remove("hidden");
+  } else {
+    playButton.classList.add("hidden");
   }
 }
 
-inputs.forEach(input => {
-  input.addEventListener('input', checkInputs);
+inputs.forEach((input) => {
+  input.addEventListener("input", checkInputs);
 });
 
 /* function startGame() {
